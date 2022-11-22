9aecbc35710d53472096a160dbc8bb1ec819288e
Creaper001 <henricanteiro@gmail.com>
2022-11-22 04:33:14 -0300

    Default Route

--- api/src/router.ts
+++ api/src/router.ts
@@ -9,5 +9,8 @@ router.post("/urls", UrlController.create);
 router.get("/urls", UrlController.list);
 router.delete("/urls/:id", UrlController.remove);
 router.get("/:userHashtag/:id", UrlController.redirect);
+router.get("/", (req, res) => {
+  return res.send("API:URLM");
+});
 
 export default router;
